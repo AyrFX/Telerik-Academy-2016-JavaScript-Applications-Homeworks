0ea9b6df5a72c8650ddf58339d5054080b489e7b
AyrFX <oleg@gbg.bg>
2016-09-16 10:27:44 +0300

    Promises and asynchronous programming Homework: changed task 2 to use toastr instead a div in html

--- 01. Promises and asynchronous programming/task-2.js
+++ 01. Promises and asynchronous programming/task-2.js
@@ -1,7 +1,8 @@
 (function () {
-    var output = document.querySelector('#output');
+    //var output = document.querySelector('#output');
     var promise = new Promise(function (resolve) {
-        output.innerHTML = 'Waiting for 2000 ms...';
+        //output.innerHTML = 'Waiting for 2000 ms...';
+        toastr.info('Waiting for 2000 ms...');
         setTimeout(function () {
             resolve();
         }, 2000);
